50d43af332cca5bf38fc9f65c3902d4ec524e83a
Ernesto Canquiz <fdsoil123@gmail.com>
2022-10-31 18:15:48 -0400

    feat: My first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: ['@pinia/nuxt'],
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+  css: ['@/assets/css/main.css'],
+})
